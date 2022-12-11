565faf5a5380e1186efc43af38b7b214f6e7754b
Kasekopf <jeffreydudek@gmail.com>
2022-12-11 15:37:46 -0800

    Set properties

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,4 +1,5 @@
 import { Engine } from "grimoire-kolmafia";
+import { PropertiesManager } from "libram";
 import { getCurrentLeg, Task } from "../tasks/structure";
 import { printProfits, ProfitTracker } from "./profits";
 
@@ -21,5 +22,25 @@ export class ProfitTrackingEngine extends Engine<never, Task> {
     super.destruct();
     this.profits.save();
     printProfits(this.profits.all());
+  }
+
+  initPropertiesManager(manager: PropertiesManager): void {
+    super.initPropertiesManager(manager);
+    manager.setChoices({
+      1106: 3, // Ghost Dog Chow
+      1107: 1, // tennis ball
+      1340: 3, // Is There A Doctor In The House?
+      1341: 1, // Cure her poison
+      // June cleaver noncombats
+      1467: 1,
+      1468: 1,
+      1469: 2,
+      1470: 2,
+      1471: 1,
+      1472: 2,
+      1473: 2,
+      1474: 2,
+      1475: 1,
+    });
   }
 }
